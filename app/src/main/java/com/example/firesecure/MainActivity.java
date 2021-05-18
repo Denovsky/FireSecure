36b9632500b485bce8aacbb50772a4f6cb5a391b
Ilya_Denovsky <ilya20002282000@mail.ru>
2021-05-18 18:46:06 +0300

    n

--- app/src/main/java/com/example/firesecure/MainActivity.java
+++ app/src/main/java/com/example/firesecure/MainActivity.java
@@ -2,14 +2,78 @@ package com.example.firesecure;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.firesecure.View.ChoseDivision;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button entry_but, data_but, calc_but, setting_but;
+    private Context context;
+
+    private SharedPreferences sharedPreferences = null;
+    private SharedPreferences.Editor editor;
+
+    public static final String APP_PREFERENCES = "mySettings";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // делаем полноэкранное
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+        init();
+        if (sharedPreferences.getAll().size() == 0){
+            editor.putBoolean("Flag", true);
+        }
+        else {
+            
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.entry_but:
+                Intent intent = new Intent(context, ChoseDivision.class);
+                startActivity(intent);
+                break;
+            case R.id.data_but:
+                break;
+            case R.id.calc_but:
+                break;
+            case R.id.setting_but:
+                break;
+        }
+    }
+
+    public void init() {
+        context = this.getApplicationContext();
+
+        entry_but = (Button) findViewById(R.id.entry_but);
+        data_but = (Button) findViewById(R.id.data_but);
+        calc_but = (Button) findViewById(R.id.calc_but);
+        setting_but = (Button) findViewById(R.id.setting_but);
+
+        entry_but.setOnClickListener(this);
+        data_but.setOnClickListener(this);
+        calc_but.setOnClickListener(this);
+        setting_but.setOnClickListener(this);
+
+        sharedPreferences = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
 
     }
 }
---
+++ app/src/main/java/com/example/firesecure/View/ChoseDivision.java
@@ -0,0 +1,27 @@
+package com.example.firesecure.View;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.firesecure.R;
+
+public class ChoseDivision extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // делаем полноэкранное
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.chose_division);
+    }
+
+    private void init() {
+
+    }
+}
